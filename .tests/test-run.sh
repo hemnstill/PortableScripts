ff1778799cdd1b1f5fb5496e9c620eaf33e868dd
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-01-29 20:35:03 +0400

    +x

--- .tests/test-run.sh
+++ .tests/test-run.sh
@@ -4,12 +4,12 @@ cd "$(dirname "$0")"
 errors_count=0
 
 echo ">> Init (Bash)"
-bash ../Bash/init.sh
-bash ../Bash/run.sh
+../Bash/init.sh
+../Bash/run.sh
 
 echo ">> Test (Bash)"
 etalon_log=$(echo -e "hello, 's1 ы1'\nexit code: 42\n")
-actual_log=$(bash ../Bash/run.sh s1 ы1)
+actual_log=$(../Bash/run.sh s1 ы1)
 if [ "$etalon_log" != "$actual_log" ]; then
   errors_count=$((errors_count + 1))
   echo "<< Failed (Bash)"
