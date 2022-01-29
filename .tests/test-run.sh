26adc7457e9625598202d5e8a1552ca8087fc768
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-01-30 02:09:38 +0400

    -run.bat

--- .tests/test-run.sh
+++ .tests/test-run.sh
@@ -5,9 +5,6 @@ dp0_tools="$dp0/../.tools" && source "$dp0_tools/env_tools.sh"
 errors_count=0
 
 uname -a
-
-$is_windows_os && chcp
-
 echo ">> Init (Bash)"
 "$dp0/../Bash/init.sh"
 "$dp0/../Bash/run.sh"
