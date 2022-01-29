017fe2403aac688fce7e802b6f72161fa167b2a5
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-01-29 20:51:14 +0400

    dp0

--- .tools/env_tools.sh
+++ .tools/env_tools.sh
@@ -1,2 +1,4 @@
 export LC_ALL=en_US.UTF-8
-p7z="../.tools/7zzs" && [[ $(uname) == Windows_NT* ]] && p7z="../.tools/7z.exe"
+dp0="$(dirname "$0")"
+
+p7z="$dp0/../.tools/7zzs" && [[ $(uname) == Windows_NT* ]] && p7z="$dp0/../.tools/7z.exe"
