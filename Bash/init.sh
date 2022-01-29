d08bb9f022b5c2d65c9b4619e6adb03a682d4336
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-01-29 20:37:31 +0400

    ~

--- Bash/init.sh
+++ Bash/init.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 
 source ../.tools/env_tools.sh
 
-archive_name=./tools/busybox-win.7z && [[ $(uname) == Windows_NT* ]] && archive_name=./tools/busybox-linux.7z
-binary_name=../.tools/busybox64.exe && [[ $(uname) == Windows_NT* ]] && binary_name=../.tools/busybox
+archive_name=./tools/busybox-linux.7z && [[ $(uname) == Windows_NT* ]] && archive_name=./tools/busybox-win.7z
+binary_name=../.tools/busybox && [[ $(uname) == Windows_NT* ]] && binary_name=../.tools/busybox64.exe
 
 $p7z a $archive_name -up0q0 "$binary_name"
