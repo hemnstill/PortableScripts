03d4d6c7f52eef64dbdcc088bd43ec9f812a7697
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-01-29 19:32:41 +0400

    +1

--- .tests/test-run.sh
+++ .tests/test-run.sh
@@ -10,7 +10,7 @@ echo ">> Test (Bash)"
 etalon_log=$(echo -e "hello, 's1 ы1'\nexit code: 42\n")
 actual_log=$(../Bash/run.bat s1 ы1)
 if [ "$etalon_log" != "$actual_log" ]; then
-  ((errors_count++))
+  errors_count=$((errors_count + 1))
   echo "<< Failed (Bash)"
   echo expected: "$etalon_log"
   echo actual: "$actual_log"
