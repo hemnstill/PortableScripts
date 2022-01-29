7498342263e16311fd2f4e3a0c80eb351e606358
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-01-29 20:59:55 +0400

    version

--- Bash/init.sh
+++ Bash/init.sh
@@ -6,4 +6,7 @@ source "$dp0/../.tools/env_tools.sh"
 archive_name="$dp0/tools/busybox-linux.7z" && [[ $(uname) == Windows_NT* ]] && archive_name="$dp0/tools/busybox-win.7z"
 binary_name="$dp0/../.tools/busybox" && [[ $(uname) == Windows_NT* ]] && binary_name="$dp0/../.tools/busybox64.exe"
 
-$p7z a "$archive_name" -up0q0 "$binary_name"
+echo busybox version:
+"$binary_name"
+
+"$p7z" a "$archive_name" -up0q0 "$binary_name"
