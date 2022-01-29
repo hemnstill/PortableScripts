b7cf85c0092bf46f8303dc992349bf7f08f0c61f
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-01-30 00:45:13 +0400

    ~fix url

--- Python/init.sh
+++ Python/init.sh
@@ -15,8 +15,9 @@ else
   zstd="zstd"
 fi
 
-download_url_prefix=https://github.com/indygreg/python-build-standalone/releases/download/20211017/cpython-3.10.0-x86_64
-download_url="$download_url_prefix-unknown-linux-musl-noopt-20211017T1616.tar.zst" && $is_windows_os && download_url="$download_url_prefix-pc-windows-msvc-shared-pgo-20211017T1616.tar.zst"
+linux_download_url=https://github.com/indygreg/python-build-standalone/releases/download/20211017/cpython-3.10.0-x86_64-unknown-linux-musl-noopt-20211017T1616.tar.zst
+windows_download_url=https://github.com/indygreg/python-build-standalone/releases/download/20211017/cpython-3.10.0-x86_64-pc-windows-msvc-shared-pgo-20211017T1616.tar.zst
+download_url="$linux_download_url" && $is_windows_os && download_url="$windows_download_url"
 cpython_zip="$runtime_tools/cpython-linux.tar.zst" && $is_windows_os && cpython_zip="$runtime_tools/cpython-win.tar.zst"
 [[ ! -f "$cpython_zip" ]] && "$busybox" wget "$download_url" -O "$cpython_zip"
 
