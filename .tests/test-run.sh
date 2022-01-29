497ae6e8f2c1bf25c5dab9a9d3ee6cb2bf7e0958
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2022-01-30 02:19:25 +0400

    PowerShell (#4)
    #TODO: fix encoding

--- .tests/test-run.sh
+++ .tests/test-run.sh
@@ -4,6 +4,7 @@ dp0_tools="$dp0/../.tools" && source "$dp0_tools/env_tools.sh"
 
 errors_count=0
 
+uname -a
 echo ">> Init (Bash)"
 "$dp0/../Bash/init.sh"
 "$dp0/../Bash/run.sh"
@@ -12,8 +13,12 @@ echo ">> Init (Python)"
 "$dp0/../Python/init.sh"
 "$dp0/../Python/run.sh"
 
+echo ">> Init (PowerShell)"
+"$dp0/../PowerShell/init.sh"
+"$dp0/../PowerShell/run.sh"
+
 echo ">> Test (Bash)"
-etalon_log=$(echo -e "hello, 's1 ы1'\nexit code: 42\n")
+etalon_log=$(echo -e "Hello, 's1 ы1'\nexit code: 42\n")
 actual_log=$("$dp0/../Bash/run.sh" s1 ы1)
 if [ "$etalon_log" != "$actual_log" ]; then
   errors_count=$((errors_count + 1))
@@ -25,7 +30,7 @@ else
 fi
 
 echo ">> Test (Python)"
-etalon_log=$(echo -e "hello, '['s1', 'ы1']'\nexit code: 42\n")
+etalon_log=$(echo -e "Hello, '['s1', 'ы1']'\nexit code: 42\n")
 actual_log=$("$dp0/../Python/run.sh" s1 ы1)
 # crlf fix
 $is_windows_os && actual_log=$(echo "$actual_log" | dos2unix)
@@ -36,6 +41,20 @@ if [ "$etalon_log" != "$actual_log" ]; then
   echo actual: "$actual_log"
 else
   echo "<< Passed (Python)"
+fi
+
+echo ">> Test (PowerShell) #TODO: fix_encoding"
+etalon_log=$(echo -e "Hello, 's1 todo_fix_encoding1'\nexit code: 42\n")
+actual_log=$("$dp0/../PowerShell/run.sh" s1 todo_fix_encoding1)
+# crlf fix
+$is_windows_os && actual_log=$(echo "$actual_log" | dos2unix)
+if [ "$etalon_log" != "$actual_log" ]; then
+  errors_count=$((errors_count + 1))
+  echo "<< Failed (PowerShell)"
+  echo expected: "$etalon_log"
+  echo actual: "$actual_log"
+else
+  echo "<< Passed (PowerShell)"
 fi
 
 echo Errors: "$errors_count"
