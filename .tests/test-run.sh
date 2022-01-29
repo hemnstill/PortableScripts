9cd7353d0cb94739889bbdca1425ff2f07f566de
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-01-29 19:19:23 +0400

    bash test-run.sh

---
+++ .tests/test-run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+errors_count=0
+
+echo ">> Test (Bash)"
+etalon_log=$(echo -e "hello, 's1 ы1'\nexit code: 42\n")
+actual_log=$(../Bash/run.bat s1 ы1)
+if [ "$etalon_log" != "$actual_log" ]; then
+  ((errors_count++))
+  echo "<< Failed (Bash)"
+  echo expected: "$etalon_log"
+  echo actual: "$actual_log"
+else
+  echo "<< Passed (Bash)"
+fi
+
+echo Errors: "$errors_count"
+exit "$errors_count"
