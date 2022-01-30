15068d53851129e7b1fab98b88a721e8e6025db5
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-01-30 11:57:00 +0400

    alpine nodejs

--- Nodejs/init.sh
+++ Nodejs/init.sh
@@ -6,6 +6,7 @@ runtime_tools="$dp0/tools"
 mkdir -p "$runtime_tools"
 
 linux_download_url=https://nodejs.org/dist/v16.13.2/node-v16.13.2-linux-x64.tar.xz
+$is_alpine_os && linux_download_url=https://github.com/oznu/alpine-node/releases/download/16.13.2/node-v16.13.2-linux-x86_64-alpine.tar.gz
 windows_download_url=https://nodejs.org/dist/v16.13.2/node-v16.13.2-win-x64.zip
 download_url="$linux_download_url" && $is_windows_os && download_url="$windows_download_url"
 runtime_zip="$runtime_tools/node-linux.tar.xz" && $is_windows_os && runtime_zip="$runtime_tools/node-win.zip"
