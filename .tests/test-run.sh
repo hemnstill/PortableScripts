b0f543db81dae7a0ce87e664dadf84b749580cb6
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-02-02 01:37:13 +0400

    ash

--- .tests/test-run.sh
+++ .tests/test-run.sh
@@ -13,15 +13,15 @@ echo is_windows: "$is_windows_os", is_alpine: "$is_alpine_os"
 function test_init() {
   local runtime_name="$1"
   echo ">> Init ($runtime_name)"
-  "$dp0/../$runtime_name/init.sh"
-  "$dp0/../$runtime_name/run.sh"
+  "$busybox" ash "$dp0/../$runtime_name/init.sh"
+  "$busybox" ash "$dp0/../$runtime_name/run.sh"
 }
 
 function test_stdout() {
   local runtime_name="$1"
   echo ">> Test ($runtime_name)"
   local etalon_log=$(echo -e "$2")
-  local actual_log=$("$dp0/../$runtime_name/run.sh" "$3")
+  local actual_log=$("$busybox" ash "$dp0/../$runtime_name/run.sh" "$3")
   # crlf fix
   $is_windows_os && actual_log=$(echo "$actual_log" | dos2unix)
   if [ "$etalon_log" != "$actual_log" ]; then
