b76e8677d7ed144d416a29657f42e4328ce33a43
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2022-04-17 23:47:48 +0400

    Self batch (#20)
    polyglot, self extracted `.bat` file
    - can run scripts with embedded busybox64.exe with cmd.exe on windows
    - can run from `sh` on linux (looks like future '.run' files)
    - busybox64 is base64 encoded and embed into .bat file
    - busybox64 is decoded with certutil.exe on windows

---
+++ SelfBatch/init.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+dp0_tools="$dp0/../.tools" && source "$dp0_tools/env_tools.sh"
+set -e
+
+content_before="$dp0/.tmp/content_before.sh"
+content_after="$dp0/.tmp/content_after.sh"
+content_busybox="$dp0/.tmp/_content.txt"
+
+{
+  echo -----BEGIN CERTIFICATE-----
+  base64 "$busybox"
+  echo -----END CERTIFICATE-----
+} > "$content_busybox"
+
+{ printf ': '\''"
+@echo off
+
+set busybox_local=%%~dp0busybox64_v1.35.0.exe
+if not exist %%busybox_local%% (
+  certutil.exe -f -decode "%%~f0" "%%busybox_local%%"
+)
+
+goto :entrypoint
+
+'
+} > "$content_before"
+
+{ printf '
+:entrypoint
+echo Hello, Batch %%*
+"%%busybox_local%%" sh "%%~f0" %%*
+exit /b 42
+"'\''
+
+echo Hello, Bash "$@"
+exit 42
+
+'
+} > "$content_after"
+
+cat "$content_before" "$content_busybox" "$content_after" > "$dp0/run.bat"
---
+++ SelfBatch/test-run.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+dp0_tools="$dp0/../.tools" && source "$dp0_tools/env_tools.sh"
+
+errors_count=0
+
+echo ">> env:"
+uname -a
+echo is_windows: "$is_windows_os", is_alpine: "$is_alpine_os"
+"$busybox" | head -n 1
+"$p7z" | head -n 2 | tail -n 1
+
+function test_init() {
+  local runtime_name="$1"
+  echo ">> Init ($runtime_name)"
+  "$dp0/init.sh"
+  sh "$dp0/run.bat"
+}
+
+function test_init_batch() {
+  local runtime_name="$1"
+  echo ">> Init ($runtime_name)"
+  "$dp0/init.sh"
+  cmd.exe /c call "$dp0/run.bat"
+}
+
+function test_stdout() {
+  local runtime_name="$1"
+  echo ">> Test ($runtime_name)"
+  local etalon_log=$(echo -e "$2")
+  local actual_log=$("$busybox" sh "$dp0/run.bat" "$3")
+  # crlf fix
+  $is_windows_os && actual_log=$(echo "$actual_log" | dos2unix)
+  if [ "$etalon_log" != "$actual_log" ]; then
+    errors_count=$((errors_count + 1))
+    echo "<< Failed ($runtime_name)"
+    echo expected: "$etalon_log"
+    echo actual: "$actual_log"
+  else
+    echo "<< Passed ($runtime_name)"
+  fi
+}
+
+function test_stdout_batch() {
+  local runtime_name="$1"
+  echo ">> Test ($runtime_name)"
+  local etalon_log=$(echo -e "$2")
+  local actual_log=$(cmd.exe /c call "$dp0/run.bat" "$3")
+  # crlf fix
+  $is_windows_os && actual_log=$(echo "$actual_log" | dos2unix)
+  if [ "$etalon_log" != "$actual_log" ]; then
+    errors_count=$((errors_count + 1))
+    echo "<< Failed ($runtime_name)"
+    echo expected: "$etalon_log"
+    echo actual: "$actual_log"
+  else
+    echo "<< Passed ($runtime_name)"
+  fi
+}
+
+test_init "SelfBash"
+test_stdout "SelfBash" "Hello, Bash s1 todo_fix_encoding1\n" "s1 todo_fix_encoding1"
+
+# 'certutil.exe' is not recognized as an internal or external command
+if [ "$is_nanoserver_os" != true ]; then
+  $is_windows_os && test_init_batch "SelfBatch"
+  $is_windows_os && test_stdout_batch "SelfBatch" "Hello, Batch \"s1 todo_fix_encoding1\"\nHello, Bash s1 todo_fix_encoding1\n" "s1 todo_fix_encoding1"
+fi
+
+echo Errors: "$errors_count"
+exit $errors_count
