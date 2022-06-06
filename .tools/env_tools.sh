98d5cb2b161f8ec112c5c3c750c2c6c8c704b6ec
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-06-06 22:39:28 +0400

    add assert_stdout

--- .tools/env_tools.sh
+++ .tools/env_tools.sh
@@ -9,3 +9,21 @@ busybox_filename=busybox && $is_windows_os && busybox_filename=busybox64.exe
 busybox="$dp0_tools/$busybox_filename"
 
 bsdtar="$dp0_tools/bsdtar" && $is_windows_os && bsdtar="$dp0_tools\bsdtar.exe"
+
+log_env() {
+  echo ">> env:"
+  uname -a
+  echo is_windows: "$is_windows_os", is_alpine: "$is_alpine_os"
+  "$busybox" | head -n 1
+  "$p7z" | head -n 2 | tail -n 1
+}
+
+assert_stdout() {
+  local runtime_name="$1"
+  echo ">> Test ($runtime_name)"
+  local etalon_log=$(echo -e "$2")
+  local actual_log=$("$dp0_tools/../$runtime_name/run.sh" "$3")
+  # crlf fix
+  $is_windows_os && actual_log=$(echo "$actual_log" | dos2unix)
+  assetEquals "$etalon_log" "$actual_log"
+}
