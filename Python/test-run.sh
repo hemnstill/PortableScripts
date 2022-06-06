d71e853413bb19ee51c6db9479d4ebc11e8a29f1
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2022-06-06 22:51:48 +0400

    python shunit2 (#26)
    * add `assert_stdout` and `log_env` functions

--- Python/test-run.sh
+++ Python/test-run.sh
@@ -2,40 +2,20 @@
 dp0="$(realpath "$(dirname "$0")")"
 dp0_tools="$dp0/../.tools" && source "$dp0_tools/env_tools.sh"
 
-errors_count=0
+oneTimeSetUp() {
+  log_env
+}
 
-echo ">> env:"
-uname -a
-echo is_windows: "$is_windows_os", is_alpine: "$is_alpine_os"
-"$busybox" | head -n 1
-"$p7z" | head -n 2 | tail -n 1
-
-function test_init() {
-  local runtime_name="$1"
-  echo ">> Init ($runtime_name)"
-  "$dp0/init.sh" || exit 1
+test_init() {
+  "$dp0/init.sh"
+  assertEquals 0 $?
   "$dp0/run.sh"
+  assertEquals 42 $?
 }
 
-function test_stdout() {
-  local runtime_name="$1"
-  echo ">> Test ($runtime_name)"
-  local etalon_log=$(echo -e "$2")
-  local actual_log=$("$dp0/../$runtime_name/run.sh" "$3")
-  # crlf fix
-  $is_windows_os && actual_log=$(echo "$actual_log" | dos2unix)
-  if [ "$etalon_log" != "$actual_log" ]; then
-    errors_count=$((errors_count + 1))
-    echo "<< Failed ($runtime_name)"
-    echo expected: "$etalon_log"
-    echo actual: "$actual_log"
-  else
-    echo "<< Passed ($runtime_name)"
-  fi
+test_stdout() {
+  assert_stdout "Python" "Hello, '['s1 ы1']'\nexit code: 42\n" "s1 ы1"
 }
 
-test_init "Python"
-test_stdout "Python" "Hello, '['s1 ы1']'\nexit code: 42\n" "s1 ы1"
-
-echo Errors: "$errors_count"
-exit $errors_count
+# Load and run shUnit2.
+source "$dp0/../.tests/shunit2/shunit2"
